a0415b6fd8fcd689a99215ae81291f69ef3c5c70
Max Makoveev <maxim.makoveev@gmail.com>
2021-10-12 13:20:28 +0300

    add First55

--- src/main/java/com/ifmo/lesson2/First55.java
+++ src/main/java/com/ifmo/lesson2/First55.java
@@ -7,11 +7,8 @@ public class First55 {
     Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
      */
     public static void main(String[] args) {
-        int array[] = new int[55];
-        array[0] = 1;
-        for (int i = 1; i < array.length; i++) {
-            array[i] = array[i-1] +2;
+        for (int i = 1; i < 55*2; i+=2) {
+            System.out.println(i);
         }
-        System.out.println(Arrays.toString(array));
     }
 }
