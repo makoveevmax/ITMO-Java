05f3b417204e767d5bcf12c874d67acaebf03663
Max Makoveev <maxim.makoveev@gmail.com>
2021-10-12 19:01:36 +0300

    add ClosestToTen

---
+++ src/main/java/com/ifmo/lesson2/ClosestToTen.java
@@ -0,0 +1,24 @@
+package com.ifmo.lesson2;
+
+public class ClosestToTen {
+    /*
+     Создать программу, выводящую на экран ближайшее к 10 из двух чисел, записанных в
+     переменные m и n. Например, среди чисел 8,5 и 11,45 ближайшее к десяти 11,45.
+     */
+    public static void main(String[] args) {
+        float m = 8.5f;
+        float n = 11.45f;
+
+        float closestToTen = closestToTen(m, n);
+
+        System.out.println(closestToTen);
+    }
+
+    public static float closestToTen(float m, float n) {
+        float magicNumber = 10f;
+        if (Math.abs(magicNumber - n) < (Math.abs(magicNumber - m))) {
+         return n;
+        } else { return m;}
+
+    }
+}
