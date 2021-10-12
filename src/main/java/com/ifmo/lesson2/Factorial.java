53889bca6e075d33288266f44d9c525950e6a694
Max Makoveev <maxim.makoveev@gmail.com>
2021-10-12 13:45:09 +0300

    add Factorial

---
+++ src/main/java/com/ifmo/lesson2/Factorial.java
@@ -0,0 +1,26 @@
+package com.ifmo.lesson2;
+
+import java.util.Scanner;
+
+public class Factorial {
+    /*
+     Создайте программу, вычисляющую факториал натурального числа n, которое
+     пользователь введёт с клавиатуры.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        long factorial = factorial(n);
+
+        System.out.println(factorial);
+    }
+
+    public static long factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else
+            return n * factorial(n - 1);
+    }
+}
