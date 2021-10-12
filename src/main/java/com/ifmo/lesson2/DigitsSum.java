2e46839fe02d197a62eafea9733aa837f11ac273
Max Makoveev <maxim.makoveev@gmail.com>
2021-10-12 19:10:12 +0300

    add DigitsSum

---
+++ src/main/java/com/ifmo/lesson2/DigitsSum.java
@@ -0,0 +1,29 @@
+package com.ifmo.lesson2;
+
+import java.util.Scanner;
+
+public class DigitsSum {
+    /*
+    Для введённого пользователем с клавиатуры натурального числа посчитайте сумму всех
+    его цифр (заранее не известно сколько цифр будет в числе).
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int digitSum = digitSum(n);
+
+        System.out.println(digitSum);
+    }
+
+    public static int digitSum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+
+        return sum;
+    }
+}
