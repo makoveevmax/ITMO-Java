8c14327469c9bec40b5d0474bed65b857200e6ff
Max Makoveev <maxim.makoveev@gmail.com>
2021-10-12 15:07:18 +0300

    add FloatRound

---
+++ src/main/java/com/ifmo/lesson2/FloatRound.java
@@ -0,0 +1,20 @@
+package com.ifmo.lesson2;
+
+public class FloatRound {
+    /*
+    В переменной n хранится вещественное число с ненулевой дробной частью.
+    Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.
+     */
+    public static void main(String[] args) {
+        float n = 12.4F;
+
+        float rounded = round(n);
+
+        System.out.println(rounded);
+    }
+
+    public static float round(float n) {
+
+        return Math.round(n);
+    }
+}
