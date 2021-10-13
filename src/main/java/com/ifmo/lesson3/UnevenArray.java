d4722025013e091fb260da08f83f41fb21af740f
Max Makoveev <maxim.makoveev@gmail.com>
2021-10-13 15:57:46 +0300

    add luckyTickets

--- src/main/java/com/ifmo/lesson3/UnevenArray.java
+++ src/main/java/com/ifmo/lesson3/UnevenArray.java
@@ -4,34 +4,34 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class UnevenArray {
-    /*
-    Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем
-    этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3
-    1)
-     */
-    public static void main(String[] args) {
-        int[] unevenArray = unevenArray();
-
-        System.out.println(Arrays.toString(unevenArray));
-    }
-
-    public static int[] unevenArray() {
-        int[] array = new int[50];
-        int count = 1;
-        for (int i = 0; i < 50; i++) {
-            array[i] = count;
-            count += 2;
-        }
-
-        System.out.println(Arrays.toString(array));
-
-        Object[] inverted = IntStream.rangeClosed(1, array.length)
-                .mapToObj(i -> array[array.length - i])
-                .toArray();
-        System.out.println(Arrays.toString(inverted));
-
-
-        System.exit(0);
-        return null;
-    }
+//    /*
+//    Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем
+//    этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3
+//    1)
+//     */
+//    public static void main(String[] args) {
+//        int[] unevenArray = unevenArray();
+//
+//        System.out.println(Arrays.toString(unevenArray));
+//    }
+//
+//    public static int[] unevenArray() {
+//        int[] array = new int[50];
+//        int count = 1;
+//        for (int i = 0; i < 50; i++) {
+//            array[i] = count;
+//            count += 2;
+//        }
+//
+//        System.out.println(Arrays.toString(array));
+//
+//        Object[] inverted = IntStream.rangeClosed(1, array.length)
+//                .mapToObj(i -> array[array.length - i])
+//                .toArray();
+//        System.out.println(Arrays.toString(inverted));
+//
+//
+//        System.exit(0);
+//        return null;
+//    }
 }
