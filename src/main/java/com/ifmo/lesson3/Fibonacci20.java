c274a9e03e4cc0d30a3e2201c0437ad2ca6ca38c
Maksim Makoveev <56920480+makoveevmax@users.noreply.github.com>
2021-10-13 19:05:05 +0300

    add Fibonacci20

---
+++ src/main/java/com/ifmo/lesson3/Fibonacci20.java
@@ -0,0 +1,26 @@
+package com.ifmo.lesson3;
+
+import java.util.Arrays;
+
+public class Fibonacci20 {
+    /*
+    Создайте массив из 20-ти первых чисел Фибоначчи и выведите его на экран. Напоминаем,
+    что первый и второй члены последовательности равны единицам, а каждый следующий —
+    сумме двух предыдущих.
+     */
+    public static void main(String[] args) {
+        int[] fibonacciNumbers = fibonacciNumbers();
+
+        System.out.println(Arrays.toString(fibonacciNumbers));
+    }
+
+    public static int[] fibonacciNumbers() {
+        int array[] = new int[20];
+        array[0] = array[1] = 1;
+        for (int i = 2; i < array.length; i++)
+            array[i] = array[i-2] + array [i-1];
+
+        return array;
+    }
+
+}
