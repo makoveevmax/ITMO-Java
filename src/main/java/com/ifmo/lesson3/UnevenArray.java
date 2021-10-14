13b6216e91483d011b8cafb0d39f94ac4d8565e3
Maksim Makoveev <56920480+makoveevmax@users.noreply.github.com>
2021-10-14 12:21:22 +0300

    add UnevenArray

--- src/main/java/com/ifmo/lesson3/UnevenArray.java
+++ src/main/java/com/ifmo/lesson3/UnevenArray.java
@@ -1,37 +1,35 @@
 package com.ifmo.lesson3;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class UnevenArray {
-//    /*
-//    Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем
-//    этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3
-//    1)
-//     */
-//    public static void main(String[] args) {
-//        int[] unevenArray = unevenArray();
-//
-//        System.out.println(Arrays.toString(unevenArray));
-//    }
-//
-//    public static int[] unevenArray() {
-//        int[] array = new int[50];
-//        int count = 1;
-//        for (int i = 0; i < 50; i++) {
-//            array[i] = count;
-//            count += 2;
-//        }
-//
-//        System.out.println(Arrays.toString(array));
-//
-//        Object[] inverted = IntStream.rangeClosed(1, array.length)
-//                .mapToObj(i -> array[array.length - i])
-//                .toArray();
-//        System.out.println(Arrays.toString(inverted));
-//
-//
-//        System.exit(0);
-//        return null;
-//    }
+    /*
+    Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем
+    этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3
+    1)
+     */
+    public static void main(String[] args) {
+        int[] unevenArray = unevenArray();
+
+        System.out.print(Arrays.toString(unevenArray));
+    }
+
+    public static int[] unevenArray() {
+        int[] array = new int[50];
+        int count = 1;
+        for (int i = 0; i < 50; i++) {
+            array[i] = count;
+            count += 2;
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        for (int i =0; i < array.length/2; i++) {
+            int temp =  array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
 }
+
