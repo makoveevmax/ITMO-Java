d9a66ac143e9fe36ac41b327f37d41cdd89df38f
Maksim Makoveev <56920480+makoveevmax@users.noreply.github.com>
2021-10-14 11:52:54 +0300

    add DivisionRemainder

--- src/main/java/com/ifmo/lesson1/DivisionRemainder.java
+++ src/main/java/com/ifmo/lesson1/DivisionRemainder.java
@@ -7,6 +7,7 @@ public class DivisionRemainder {
     Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8):
     21 / 8 = 2 и 5 в остатке
      */
+
     public static void main(String[] args) {
         int q = 21;
         int w = 8;
